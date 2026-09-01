e98abbc85a4307c43d0f014259c568cc7d254c68
Markus Palcer <markus.palcer@gmail.com>
2026-09-01 18:02:32 +0200

    move subprojects to other repos

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,11 +1,2 @@
-﻿using BenchmarkDotNet.Running;
-using Benchmarks;
-using Benchmarks.Benchmarks;
+﻿Console.WriteLine();
 
-// BenchmarkRunner.Run<FullStackEncodeBenchmark>();
-// BenchmarkRunner.Run<FullStackDecodeBenchmark>();
-// BenchmarkRunner.Run<EncodeBase83Benchmark>();
-// BenchmarkRunner.Run<DecodeBase83Benchmark>();
-BenchmarkRunner.Run<CoreEncodeBenchmark>();
-// BenchmarkRunner.Run<CoreDecodeBenchmark>();
-// BenchmarkRunner.Run<EncoderConvertBitmapBenchmark>();
--- Blurhash.System.Drawing.Common/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Blurhash.System.Drawing.Common.Test")]
-[assembly: InternalsVisibleTo("Benchmarks")]
